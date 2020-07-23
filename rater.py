1bdc69295a94b2f03ccfacd80f4e2a306ed01d5d
Ryan Bui <rykbui@ucdavis.edu>
2020-07-22 23:02:06 -0700

    Sentiment analysis

--- rater.py
+++ rater.py
@@ -1,16 +1,28 @@
 from TwitterSearch import *
-import tweepy
 from textblob import TextBlob
+import tweepy
 import pandas as pd
 import re
 #import numpy as np
 #from GUI import text
 
 
-# tweepy API authentication
+def clean_text(text):
+    text = re.sub(r'@[A-Za-z0-9]+', '', text) # Removing @ mentions
+    text = re.sub(r'#', '', text) # Removing '#'
+    text = re.sub(r'RT[\s]+', '', text) # Removing retweet symbols
+    text = re.sub(r'https?:\/\/\S+', '', text) # Removing hyper links
+    return text
+
+
+def Polarity(text):
+    return TextBlob(text).sentiment.polarity
+
+
 auth = tweepy.OAuthHandler('JUBWToPuyPfmzg8n117ZTllfB', 'lt0Psg46Nqzzaa4uel3wtSbaOyh9WiYIqx6ZH5xaExthndrsc1')
 auth.set_access_token('1172272055183728640-nLQg9fvsLVieB9BXSsJq86a6kMmR8p', '5ogC7PXA1nmlNd5FCYtNaSIhF7tyA5K7CZzNBhi8qIhv1')
-api = tweepy.API(auth, wait_on_rate_limit=True)
+
+api = tweepy.API(auth)
 
 try:
     api.verify_credentials()
@@ -19,34 +31,38 @@ except:
     print("Error during authentication")
 
 try:
-    tso = TwitterSearchOrder()
-    tso.set_keywords(['piss', 'after eating this'])
-
-    # TwitterSearch authentication
-    ts = TwitterSearch(
-            consumer_key = 'JUBWToPuyPfmzg8n117ZTllfB',
-            consumer_secret = 'lt0Psg46Nqzzaa4uel3wtSbaOyh9WiYIqx6ZH5xaExthndrsc1',
-            access_token = '1172272055183728640-nLQg9fvsLVieB9BXSsJq86a6kMmR8p',
-            access_token_secret = '5ogC7PXA1nmlNd5FCYtNaSIhF7tyA5K7CZzNBhi8qIhv1'
-        )
-
-    # for tweet in ts.search_tweets_iterable(tso):
-    #     print('@%s tweeted: %s' % (tweet['user']['screen_name'], tweet['text']))
 
     # Extract 10 tweets from selected user
-    posts = api.user_timeline(screen_name="BillGates", count=10, language="en", tweet_mode="extended")
+    posts = api.search("Hamilton", count=100, lang="en")
 
     # Print the last 5 tweets
     i = 1
-    print("Last 5 tweets: \n")
-    for tweet in posts[0:5]:
-        print(str(i) + ') ' + tweet.full_text + '\n')
+    total_sentiment = 0
+    postive_feeling = 0
+    negative_feeling = 0
+    neutral_feeling = 0
+    print("Last 100 tweets: \n")
+    for tweet in posts:
+        cleaned_tweet = clean_text(tweet.text)
+        analysis = TextBlob(cleaned_tweet)
+        print(str(i) + ') ' + cleaned_tweet)
+        print(analysis.sentiment, '\n')
+        feelings = Polarity(cleaned_tweet)
+        total_sentiment += feelings
+        if feelings > 0:
+            postive_feeling += 1
+        elif feelings < 0:
+            negative_feeling += 1
+        else:
+            neutral_feeling += 1
         i += 1
+
+    print("Overall sentiment (higher = more liked): ", total_sentiment)
+    print("Positive responses: ", postive_feeling)
+    print("Neutral responses: ", neutral_feeling)
+    print("Negative responses: ", negative_feeling)
+
+
 
 except TwitterSearchException as e:
     print(e)
-
-
-
-
-
